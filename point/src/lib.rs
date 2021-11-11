44c3640efd05eeadc507bf5eecf0efa31b9ef36f
dscalo <dansbrood@live.com>
2021-11-11 07:44:45 -0500

    adding distance method to Point

--- point/src/lib.rs
+++ point/src/lib.rs
@@ -9,6 +9,10 @@ impl Point {
     pub fn new(x: i32, y: i32) -> Point {
         Point { x: x, y: y }
     }
+
+    pub fn distance_to(&self, other: &Point) -> i32 {
+       (self.x - other.x).abs() + (self.y - other.y).abs()       
+    }
 }
 
 // owerloading + operator
@@ -37,5 +41,21 @@ mod tests {
 
         assert_eq!(result.x, 4);
         assert_eq!(result.y, 6);
+    }
+
+    #[test]
+    fn distance_between_two_points_1() {
+        let point_a = Point::new(0, 6);
+        let point_b = Point::new(6, 0);
+
+        assert_eq!(point_a.distance_to(&point_b), 12);
+    }
+
+    #[test]
+    fn distance_between_two_points_2() {
+        let point_a = Point::new(3, 4);
+        let point_b = Point::new(2, 6);
+
+        assert_eq!(point_a.distance_to(&point_b), 3);
     }
 }
