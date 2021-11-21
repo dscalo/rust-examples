04aac4915f91bdf0809a884090853d37c51b81a0
dscalo <dansbrood@live.com>
2021-11-21 09:13:21 -0500

    finishing postfix

--- RPN/src/postfix.rs
+++ RPN/src/postfix.rs
@@ -13,7 +13,7 @@ use std::collections::VecDeque;
     2.        Read a token
     3.        If it's a number add it to queue
     4.        If it's an operator
-    5.               While there's an operator on the top of the stack with greater precedence:
+    5.               While there's an operator on the top of the stack with equal or greater  precedence:
     6.                       Pop operators from the stack onto the output queue
     7.               Push the current operator onto the stack
     8.        If it's a left bracket push it onto the stack
@@ -25,26 +25,57 @@ use std::collections::VecDeque;
 */
 
 pub fn convert_to_postfix(tokens: Vec<Token>) -> Vec<Token> {
-    let mut postfix = Vec::new();
-
     let mut output = Vec::new();
     let mut operators: VecDeque<Token> = VecDeque::new();
 
     for token in tokens {
         match token {
             Token::Numb(_) => output.push(token),
-            Token::Add|Token::Subtract => {
-                while operators.front()
+            Token::Add|
+            Token::Subtract|
+            Token::Multiply|
+            Token::Divide|
+            Token::Exponent => {
+                let tok_pres = get_precedence(&token);
+                let mut optional = operators.pop_front();
+                while let Some(t) = optional {
+                    let pres = get_precedence(&t);
+                    if pres >= tok_pres {                      
+                        output.push(t);
+                        optional = operators.pop_front();
+                    } else {
+                        operators.push_front(t);
+                        optional = None;
+                    }
+                }
+                operators.push_front(token);
+            },
+            Token::LParen => operators.push_front(token),
+            Token::RParen => {
+                let mut optional = operators.pop_front();
+
+                while let Some(t) = optional {
+                    if t == Token::LParen {
+                        optional = None;
+                    } else {
+                        output.push(t);
+                        optional = operators.pop_front();
+                    }
+                }
             }
-            _ => {}
         }
     }
 
-    
-    postfix
+    let mut optional = operators.pop_front();
+    while let Some(t) = optional {
+        output.push(t);
+        optional = operators.pop_front();
+    }
+
+    output
 }
 
-fn get_precedence(operator: Token) -> u32 {
+fn get_precedence(operator: &Token) -> u32 {
     match operator {
         Token::Add | Token::Subtract => return 1,
         Token::Multiply | Token::Divide => return 2,
@@ -59,7 +90,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_converts_tokens_to_postfix_simple() {
+    fn it_converts_to_postfix_single_operator() {
         let example = vec![
             Token::Numb(4.0),
             Token::Add,
@@ -69,6 +100,63 @@ mod tests {
         let expected = vec![
             Token::Numb(4.0),
             Token::Numb(8.0),
+            Token::Add
+        ];
+
+        assert_eq!(convert_to_postfix(example), expected);
+    }
+
+    #[test]
+    fn it_converts_to_postfix_2_operators() {       
+        let example = vec![
+            Token::Numb(2.0),
+            Token::Add,
+            Token::Numb(3.0),
+            Token::Multiply,
+            Token::Numb(4.0)
+        ];
+
+        let expected = vec![
+            Token::Numb(2.0),
+            Token::Numb(3.0),
+            Token::Numb(4.0),
+            Token::Multiply,
+            Token::Add
+        ];
+
+        assert_eq!(convert_to_postfix(example), expected);
+    }
+
+    #[test]
+    fn it_converts_to_postfix_complex_equation() {
+        // 5 + 2 / (3- 8) ^ 5 ^ 2 => 5238-5^2^/+
+        let example = vec![
+            Token::Numb(5.0),
+            Token::Add,
+            Token::Numb(2.0),
+            Token::Divide,
+            Token::LParen,
+            Token::Numb(3.0),
+            Token::Subtract,
+            Token::Numb(8.0),
+            Token::RParen,
+            Token::Exponent,
+            Token::Numb(5.0),
+            Token::Exponent,
+            Token::Numb(2.0)
+        ];
+
+        let expected = vec![
+            Token::Numb(5.0),
+            Token::Numb(2.0),
+            Token::Numb(3.0),
+            Token::Numb(8.0),
+            Token::Subtract,
+            Token::Numb(5.0),
+            Token::Exponent,
+            Token::Numb(2.0),
+            Token::Exponent,
+            Token::Divide,
             Token::Add
         ];
 
